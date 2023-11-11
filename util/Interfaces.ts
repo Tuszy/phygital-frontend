f562b6f66c1c45fa974da348d9419116631914a7
Dennis T <denniro@gmail.com>
2023-11-11 13:54:24 +0100

    update PhygitalAsset abi and interface id

--- util/Interfaces.ts
+++ util/Interfaces.ts
@@ -15,4 +15,4 @@ export const LSP6KeyManagerInterface = new Interface(
 export const PhygitalAssetInterface = new Interface(PhygitalAssetArtifact.abi);
 
 // Constants
-export const INTERFACE_ID_OF_PHYGITAL_ASSET = "0xcf06c304";
+export const INTERFACE_ID_OF_PHYGITAL_ASSET = "0x06d8902b";
